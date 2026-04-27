0c2a57ca4171cab1881607ef569ec50af5a33804
tomngatipa21-dot <tomngatipa21@gmail.com>
2026-04-27 16:43:57 +1000

    Create get-users.js

---
+++ api/get-users.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { password } = req.query;
+  
+  if (password !== process.env.ADMIN_PASSWORD) {
+    return res.status(401).json({ error: 'Unauthorised' });
+  }
+
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_KEY;
+
+  try {
+    const response = await fetch(`${supabaseUrl}/rest/v1/users?select=*&order=joined_at.desc`, {
+      headers: {
+        'apikey': supabaseKey,
+        'Authorization': `Bearer ${supabaseKey}`
+      }
+    });
+
+    const data = await response.json();
+    return res.status(200).json({ users: data, total: data.length });
+  } catch(e) {
+    return res.status(500).json({ error: e.message });
+  }
+}
